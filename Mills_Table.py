c87a9ce170f5e4088c87b2cd03f431bf193ddc23
scabbypython <108943894+scabbypython@users.noreply.github.com>
2022-07-18 15:06:39 -0400

    update Mills

--- Mills_Table.py
+++ Mills_Table.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-file = 'Mills_Table.csv'
+file = 'Mills_Table_data.csv'
 df = pd.read_csv(file, sep = ',', parse_dates= ['index'],encoding='utf-8-sig', usecols= ['index', 'rain', 'temp'],)    
 df['duration'] = df['index'].dt.month
 
@@ -17,37 +17,37 @@ df['day'] = df['index'].dt.normalize()
 session_map = df[df['duration'].astype(bool)].groupby('day')['block'].nunique()
 hour_map = df[df['duration'].astype(bool)].groupby('day')['rain'].count()
 
-# map to original dataframe
+# map to original df
 df['sessions'] = df['day'].map(session_map)
-df['rain(5min)'] = df['day'].map(hour_map)
+df['rain_5min'] = df['day'].map(hour_map)
 
 # calculate dfult
-df = df.groupby(['day', 'rain(5min)', 'temp', 'sessions'], as_index=False)['rain'].median()
-df['rain(hours)'] = df['rain(5min)'] / df['sessions']/12
+df = df.groupby(['day', 'rain_5min', 'temp', 'sessions'], as_index=False)['rain'].median()
+df['rain_hours'] = df['rain_5min'] / df['sessions']/12
 
 
-df['Event'] = df['rain(hours)'].apply(lambda x: 'infection' if x >= 6 else '')
+lesion_17 = df.loc[((df['temp'] >= 43) & (df['temp'] < 45)) & ((df['rain_hours'] > 18))] 
+lesion_17 = df.loc[((df['temp'] >= 45) & (df['temp'] < 46)) & ((df['rain_hours'] > 15))]
+lesion_17 = df.loc[((df['temp'] >= 46) & (df['temp'] < 48)) & ((df['rain_hours'] > 13))]
+lesion_17 = df.loc[((df['temp'] >= 48) & (df['temp'] < 50)) & ((df['rain_hours'] > 12))]
+lesion_16 = df.loc[((df['temp'] >= 50) & (df['temp'] < 52)) & ((df['rain_hours'] > 11))]
+lesion_15 = df.loc[((df['temp'] >= 52) & (df['temp'] < 54)) & ((df['rain_hours'] >  9))]
+lesion_14 = df.loc[((df['temp'] >= 54) & (df['temp'] < 57)) & ((df['rain_hours'] > 8))]
+lesion_12_13 = df.loc[((df['temp'] >= 57) & (df['temp'] < 60)) & ((df['rain_hours'] > 7))]
+lesion_9_10 = df.loc[((df['temp'] >= 60) & (df['temp'] < 76)) & ((df['rain_hours'] > 6))]
 
 
 
-# Mill's Table - need to compare temp column to the rain column
-dfObj = pd.DataFrame(df, columns = ['day', 'rain(5min)', 'temp', 'sessions', 'rain', 'rain(hours)', 'Event', 'prognosis'])
+lesion_17.insert(6, "result", '17')
+lesion_16.insert(6, "result", '16')
+lesion_15.insert(6, "result", '15')
+lesion_14.insert(6, "result", '14')
+lesion_12_13.insert(6, "result", '12_13')
+lesion_9_10.insert(6, "result", '9_10')
 
-df['prognosis'] = df['day'].map(hour_map)
-
-prognosis = dfObj[(dfObj['temp'] > 43) & (dfObj['rain'] > 18) ]
-
-
-
-df['prognosis'] = df['prognosis'].apply(lambda x: '' if x >= 1 else 'lesions in 17 days')
-    
-print("" , prognosis, sep='lesions in 17 days')
+print(lesion_17, lesion_16, lesion_15, lesion_14, lesion_12_13, lesion_9_10)
 
 
-#print(df)
 
-#     I think instead of days, it may be better to look at the whole thing as one continous time period and look at 
-#     each individual "session (wetness period)" as a separate event to see if it is greater than 6 hours.
-#     It may make more sense to ditch the polynomial and just code in the actual revised Mill's Table.
 
 #     Mill's table derived from: https://extension.psu.edu/tree-fruit-disease-an-apple-scab-review
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-file = 'Mills_Table_data_short.csv'
+file = 'Mills_Table_data.csv'
 df = pd.read_csv(file, sep = ',', parse_dates= ['index'],encoding='utf-8-sig', usecols= ['index', 'rain', 'temp'],)    
 df['duration'] = df['index'].dt.month
 
@@ -25,22 +25,25 @@ df['rain_5min'] = df['day'].map(hour_map)
 df = df.groupby(['day', 'rain_5min', 'temp', 'sessions'], as_index=False)['rain'].median()
 df['rain_hours'] = df['rain_5min'] / df['sessions']/12
 
-#df = df[np.logical_and(df['temp'] >= 43, df['rain_hours'] > 18)]
-result = df[((df['temp'] >=54) & (df['temp'] < 53)) & (df['rain_hours']) >= 1 & (df['rain_hours'] < 2)]
 
-df['result'] = df['day'].map(hour_map)
-
-print(df)
-
-
-#df['Event'] = df['rain_hours'].apply(lambda x: 'infection' if x >= 6 else '')
- 
-
-
-
-# filter df
+lesion_17 = df.loc[((df['temp'] >= 43) & (df['temp'] < 45)) & ((df['rain_hours'] > 18))] 
+lesion_17 = df.loc[((df['temp'] >= 45) & (df['temp'] < 46)) & ((df['rain_hours'] > 15))]
+lesion_17 = df.loc[((df['temp'] >= 46) & (df['temp'] < 48)) & ((df['rain_hours'] > 13))]
+lesion_17 = df.loc[((df['temp'] >= 48) & (df['temp'] < 50)) & ((df['rain_hours'] > 12))]
+lesion_16 = df.loc[((df['temp'] >= 50) & (df['temp'] < 52)) & ((df['rain_hours'] > 11))]
+lesion_15 = df.loc[((df['temp'] >= 52) & (df['temp'] < 54)) & ((df['rain_hours'] >  9))]
+lesion_14 = df.loc[((df['temp'] >= 54) & (df['temp'] < 57)) & ((df['rain_hours'] > 8))]
+lesion_12_13 = df.loc[((df['temp'] >= 57) & (df['temp'] < 60)) & ((df['rain_hours'] > 7))]
+lesion_9_10 = df.loc[((df['temp'] >= 60) & (df['temp'] < 76)) & ((df['rain_hours'] > 6))]
 
 
 
+lesion_17.insert(6, "result", '17')
+lesion_16.insert(6, "result", '16')
+lesion_15.insert(6, "result", '15')
+lesion_14.insert(6, "result", '14')
+lesion_12_13.insert(6, "result", '12_13')
+lesion_9_10.insert(6, "result", '9_10')
 
-#print(df)
+print(lesion_17, lesion_16, lesion_15, lesion_14, lesion_12_13, lesion_9_10)
+
