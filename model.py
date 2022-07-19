b59f420b78f9b357aefe9d81991bb8bdb82ff2bd
scabbypython <108943894+scabbypython@users.noreply.github.com>
2022-07-19 13:17:15 -0400

    Update model.py

--- model.py
+++ model.py
@@ -2,21 +2,26 @@
 import pandas as pd
 import numpy as np
 
+
 file = 'Mills_Table_data.csv'
 df = pd.read_csv(file, sep = ',', parse_dates= ['index'],encoding='utf-8-sig',dayfirst=True, usecols= ['index', 'rain', 'temp'],)    
+
+
+df.index = pd.to_datetime(df.index)
+
+
+# create a new column for the duration
 df['duration'] = df['index'].dt.month
 df.resample('H', convention='start').sum()
-# create a new column for the duration
+
 df['duration'] = df['rain'].apply(lambda x: '' if x <= 90 else '0.0833333333333333')
 
 
 # create helper columns defining contiguous blocks and day
 df['block'] = (df['duration'].astype(bool).shift() != df['duration'].astype(bool)).cumsum()
 df['day'] = df['index'].dt.normalize()
-results = df.groupby('index').agg({'temp': ['mean', 'min', 'max']})
-  
-print("Mean, min, and max rains of Top Speed grouped by Vehicle Type")
-print(results)
+
+df['unique_event'] = df['block']
 # group by day to get unique block count and rain count
 session_map = df[df['duration'].astype(bool)].groupby('day')['block'].nunique()
 hour_map = df[df['duration'].astype(bool)].groupby('day')['rain'].count()
@@ -30,7 +35,9 @@ df = df.groupby(['day', 'rain_5min', 'temp', 'sessions'], as_index=False)['rain'
 df['rain_hours'] = df['rain_5min'] / df['sessions']/12
 
 
-
+results = df.groupby('sessions').agg({'temp': ['mean', 'min', 'max']})
+print(results)
+print (df)
 lesion_17 = df.loc[((df['temp'] >= 43) & (df['temp'] < 45)) & ((df['rain_hours'] > 18))] 
 lesion_17 = df.loc[((df['temp'] >= 45) & (df['temp'] < 46)) & ((df['rain_hours'] > 15))]
 lesion_17 = df.loc[((df['temp'] >= 46) & (df['temp'] < 48)) & ((df['rain_hours'] > 13))]
@@ -50,4 +57,4 @@ lesion_14.insert(6, "result", '14')
 lesion_12_13.insert(6, "result", '12_13')
 lesion_9_10.insert(6, "result", '9_10')
 
-print(lesion_17, lesion_16, lesion_15, lesion_14, lesion_12_13, lesion_9_10)
+#print(lesion_17, lesion_16, lesion_15, lesion_14, lesion_12_13, lesion_9_10)
