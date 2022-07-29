638f6df925c547cc8d174ca15759fdf2eff12425
scabbypython <108943894+scabbypython@users.noreply.github.com>
2022-07-28 22:54:46 -0400

    update

---
+++ Working_model/import datetime as dt.py
@@ -0,0 +1,17 @@
+import datetime as dt
+
+
+
+#import software libraries
+import pandas as pd
+import numpy as np
+
+
+#read file
+file = 'C:\\Users\\proco\\Documents\\GitHub\\AutoMills\\Working_model\\Working_model\infection_comparison.xlsx'
+
+df = pd.read_csv(file, sep = ',', parse_dates=['index'], usecols= ['index', 'rain', 'temp'])
+
+dates = [('index')]
+x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
+y = range(len(x)) # many thanks to Kyss Tao for setting me straight here
