6396580b39a7c6166dd8c9bad0062fd0d5e9547d
scabbypython <108943894+scabbypython@users.noreply.github.com>
2022-08-05 01:11:15 -0400

    Update stenvand_model_rersample_to_hour.py

--- Stenvand/5Min_model/stenvand_model_rersample_to_hour.py
+++ Stenvand/5Min_model/stenvand_model_rersample_to_hour.py
@@ -1,11 +1,12 @@
 # This code is designed to implement the Mill's Apple Scab model on weather station data.
-# It is designed for a 5 min data source which is resampled to an hourly.
-# https://www.canr.msu.edu/uploads/files/Research_Center/NW_Mich_Hort/Pests_IPM_Mgmt/Apple_Scab_1_Chart.pdf
+# It needs a data source in 5 min increments which will be resampled to hourly readings .
+# https://www.canr.msu.edu/uploads/files/Research_Center/NW_Mich_Hort/Pests_IPM_Mgmt/Apple_Scab_Infection_Chart.pdf
+
 
 #import software libraries
 import pandas as pd
 import numpy as np
-
+import textwrap
 #display all rows when printing the dataframe
 #pd.set_option('display.max_rows', None)
 
@@ -39,15 +40,15 @@ df['duration'] = df['rain'].apply(lambda x: '1' if x >= 90 else '')
 #create rain_block column 
 df['rain_block'] = (df['duration'].astype(bool).shift() != df['duration'].astype(bool)).cumsum()
 
-#group by unique events map session and 5 minute rain increments back to dataframe
+#group by unique events map session and hour rain increments back to dataframe
 session_map = df[df['duration'].astype(bool)].groupby('rain_block')['rain_block'].nunique()
 hour_map = df[df['duration'].astype(bool)].groupby('rain_block')['rain'].count()
 df['sessions'] = df['rain_block'].map(session_map)
-df['rain_5min'] = df['rain_block'].map(hour_map)
+df['rain_hour'] = df['rain_block'].map(hour_map)
 
-#create rain_hours column
-df = df.groupby(['index','rain_block', 'rain_5min','temp', 'sessions'], as_index=False)['rain'].median()
-df['rain_hours'] = df['rain_5min'] / df['sessions']
+#create rain_hours column *switched from median to mean 8/5/2022
+df = df.groupby(['index','rain_block', 'rain_hour','temp', 'sessions'], as_index=False)['rain'].mean()
+df['rain_hours'] = df['rain_hour'] / df['sessions']
 
 #calculate min temp of each rain_block
 temp_results_min = df.groupby('rain_block').agg({'temp': ['min']})
@@ -64,8 +65,8 @@ df['rain_hour_block']=rain_hour_block
 #calculate average temp; (min+max)/2
 df['avg_temp']=(df['temp_results_max'] + df['temp_results_min'])/2
 
-#create new column for index (index2)
-df['index2']=df.groupby('rain_block').agg({'index': ['first']})
+#create new column for index (infection dates:)
+df['infection dates:']=df.groupby('rain_block').agg({'index': ['first']})
 
 #drop rows with NaN values
 df=df.dropna(axis=0)
@@ -164,7 +165,7 @@ def mills_table(avg_temp, rain_hour_block):
                      
     return results        
             
-# run the mills_table function on the data      
+# run the mills_table function on the .csv file    
 df['lesion_result'] = df[['avg_temp', 'rain_hour_block']].apply(lambda x : mills_table(*x), axis=1)
 
 df['lesion_result'] = df['lesion_result'].astype(int)
@@ -173,26 +174,48 @@ df['lesion_result'] = df['lesion_result'].astype(int)
 # replace any NaN data with empty string
 df = df.replace(np.nan, '')
 
-df['index2'] = pd.to_datetime(df['index2'])
+df['infection_dates:'] = pd.to_datetime(df['infection dates:'])
 
 
 # omit zero values in lesion_result column
 df= df[df['lesion_result'] != 0]
 
 # create temp variable to add days for 1 date
-temp = df['lesion_result'].apply(np.ceil).apply(lambda x: pd.Timedelta(x, unit='D'))
+#temp = df['lesion_result'].apply(np.ceil).apply(lambda x: pd.Timedelta(x, unit='D'))
 
 
 #erase index colum for printing 
 blankIndex=[''] * len(df)
 df.index=blankIndex
 
+print(df)
 
 # print Mills Table Results results
 print('')
 print('')
+print('')
+print('')
 print('Mills Table Results:')
+print('Stenvand et al. Model (1997)')
+print('Last Revised: 5-01-03')
 print('')
 print('')
-
-print(df[['index2']])
+print(df[['infection dates:']])
+print('')
+print('')
+value = """notes: The infection period is considered to start at the beginning of the rain. Symptoms, if the
+infection is successful, will generally appear after 9 days incubation with average daily temperatures
+at 60 F and after 16 days or more with average daily temperatures below 50F."""
+  
+# Wrap this text.
+wrapper = textwrap.TextWrapper(width=50)
+  
+word_list = wrapper.wrap(text=value)
+  
+# Print each line.
+for element in word_list:
+    print(element)
+print('')
+print('')
+print('')
+print('')
